b92082e4550d67dff8d6e1ff27e5eef8317b9c13
Paul Masalskas <36967319+Linkitu7@users.noreply.github.com>
2023-10-02 11:54:30 +0000

    updated keys

--- utils.py
+++ utils.py
@@ -36,7 +36,7 @@ def load_chain():
     You are an AI assistant for answering questions about the AI Clones project.
     You are given the following extracted parts of a long document and a question. Provide a conversational answer.
     If you don't know the answer, just say 'Sorry, I don't know ... 😔. 
-    Don't try to make up an answer.
+    Don't try to make up an answer. Keep your answers super super concise.
     If the question is not about AI Clones, politely inform them that you are tuned to only answer questions about AI Clones.
     
     {context}
